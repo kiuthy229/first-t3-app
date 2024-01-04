4e750d5958e2b8ff1c0c560c61376187b3bd9205
kiuthy229 <nguynmaikiuthy@gmail.com>
2024-01-04 09:55:31 +0700

    fix: run build errors

--- src/components/GetSanctionResult.tsx
+++ src/components/GetSanctionResult.tsx
@@ -1,26 +1,21 @@
-"use client";
-
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-import { api } from "~/trpc/react";
 
 export function GetSanctionResult() {
-  const router = useRouter();
   const [name, setName] = useState("");
 
-  const getSanctionResult = api.sanction.getSanctionResult.useMutation({
-    onSuccess: () => {
-      router.refresh();
-      setName("");
-    },
-  });
+  // const getSanctionResult = api.sanction.getSanctionResult.useMutation({
+  //   onSuccess: () => {
+  //     router.refresh();
+  //     setName("");
+  //   },
+  // });
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        getSanctionResult.mutate({ name });
+        // getSanctionResult.mutate({ name });
       }}
       className="flex flex-col gap-2"
     >
@@ -31,13 +26,13 @@ export function GetSanctionResult() {
         onChange={(e) => setName(e.target.value)}
         className="w-full rounded-full px-4 py-2 text-black"
       />
-      <button
+      {/* <button
         type="submit"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
         disabled={createPost.isLoading}
       >
         {createPost.isLoading ? "Submitting..." : "Submit"}
-      </button>
+      </button> */}
     </form>
   );
 }
